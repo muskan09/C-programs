878ed6dc5632477cb5f36904027c10cbf916c24c
muskan09 <muskankalsi9@gmail.com>
2018-11-15 22:36:25 +0530

    C codes

---
+++ cycswap.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main()
+{
+int i,j,k,l,x,y;
+i=1;
+j=2;
+k=3;
+l=4;
+for(i=1;i<j;i++)
+{
+for(j=2;j<k;j++)
+{
+for(k=3;k<l;k++)
+{
+for(l=4;l<100;l++);
+{
+x=i+j+k;
+x=l;
+printf("value of l is %d \n",l);
+printf("value of k is %d \n",k);
+printf("value of j is %d \n",j);
+printf("value of i is %d \n",i);
+
+}
+
+}
+
+}
+
+}
+
+
+
+return 0;
+}
