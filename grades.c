949fad8821bbeb2420d4c46055756e93e879309d
muskan09 <muskankalsi9@gmail.com>
2018-11-15 22:38:32 +0530

    C codes

---
+++ grades.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+float a,b,c,d,e;
+printf("Enter maths marks");
+scanf("%f",&a);
+printf("Enter eng marks");
+scanf("%f",&b);
+printf("Enter sci marks");
+scanf("%f",&c);
+printf("Enter french marks");
+scanf("%f",&d);
+printf("Enter sst marks");
+scanf("%f",&e);
+return 0;
+}
