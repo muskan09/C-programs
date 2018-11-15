3632591bef7b73755f2c2fd05a7a3e47966945dc
muskan09 <muskankalsi9@gmail.com>
2018-11-15 22:35:26 +0530

    C codes

---
+++ ary1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int arr[100];
+int i,n;
+printf("size of array");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&arr[i]);
+}
+for(i=0;i<n;i++)
+{
+printf("%d",arr[i]);
+}
+return 0;
+}
