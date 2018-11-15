d22f2cd27d7aecb9f5539d0a49aae2049f8ee98c
muskan09 <muskankalsi9@gmail.com>
2018-11-15 22:42:15 +0530

    C codes

---
+++ piggy.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c,d,e;
+printf("Enter a rupee 1 a");
+scanf("%d",&a);
+printf("Enter a rupee 2 b");
+scanf("%d",&b);
+printf("Enter a rupee 5 c");
+scanf("%d",&c);
+printf("Enter a rupee 10 d");
+scanf("%d",&d);
+e=(a*1)+(b*2)+(c*5)+(d*10);
+printf("%d",e);
+return 0;
+}
