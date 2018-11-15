facd5c823cf476a14e0c405b98c7b8c5de78d844
muskan09 <muskankalsi9@gmail.com>
2018-11-15 22:38:39 +0530

    C codes

---
+++ great2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+printf("Enter a number a");
+scanf("%d",&a);
+printf("Enter a number b");
+scanf("%d",&b);
+(a>b)?printf("a is greater"):printf("b is greater");
+return 0;
+}
