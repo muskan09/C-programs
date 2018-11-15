88b4e9f2e86db11844a5b6ac926487174800a577
muskan09 <muskankalsi9@gmail.com>
2018-11-15 22:47:09 +0530

    C codes

---
+++ trial.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+//int main()
+//{
+/*
+//factorial
+int i,n,fact;
+scanf("%d",&n);
+fact=1;
+for(i=1;i<=n;i++)
+{
+fact=fact*i;
+}
+printf("fact is %d",fact);
+return 0;
+*/
+/*
+//ascii
+char c;
+scanf("%c",&c);
+printf("%d",c);
+*/
+
+
+int main() {
+	// your code here
+	int n;
+        printf("initial digit");
+	scanf("%d \n",&n);
+        printf("%d \n",n);
+        while( n!=42 && n<100)
+		
+                {
+			scanf("%d \n",&n);
+                        if(n!=42)
+                        {
+			printf("%d \n",n);
+                          }
+		}
+	
+
+	
+	
+
+
+return 0;
+}
+
