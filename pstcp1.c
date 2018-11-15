f68128a49a1dd92f17bb38489233d16daf8ffdcb
muskan09 <muskankalsi9@gmail.com>
2018-11-15 22:43:08 +0530

    C codes

---
+++ pstcp1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int i ;
+printf ( "Enter either 1 or 2 " ) ;
+scanf ( "%d", &i ) ;
+if ( i == 1 )
+printf ( "You would go to heaven !" ) ;
+else
+{
+if ( i == 2 )
+printf ( "Hell was created with you in mind" ) ;
+else
+printf ( "How about mother earth !" ) ;
+}
+return 0;
+}
