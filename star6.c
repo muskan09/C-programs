19af8fca30c703d8bc886463ea1586396e0ad873
muskan09 <muskankalsi9@gmail.com>
2018-11-15 22:45:54 +0530

    C codes

---
+++ star6.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int i,j,k;
+printf("Enter number of rows: \n");
+scanf("%d",&k);
+for(i=1; i<=k; i++)
+{
+   for(j=1; j<=k; j++)
+   {
+   if(i==1||i==k||j==1||j==k||i==j)
+   printf("*");
+   else
+   printf(" ");
+   }
+printf("\n");
+}
+return 0;
+}
